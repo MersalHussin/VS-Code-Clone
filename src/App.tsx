c2dfb6b5743083dfd4132af0f026ad5dcc7bdb24
mersalhussin <mersalhussin@gmail.com>
2026-01-09 23:54:52 +0200

    Iterate Through File Tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import FileComponent from "./components/FileComponent";
 import FolderComponent from "./components/FolderComponent";
+// import FolderComponent from "./components/FolderComponent";
+import { fileTree } from "./data/fileTree";
 
 function App() {
   return (
     <>
-      <FileComponent fileName="Index.tsx"/>
-      <FileComponent fileName="File.tsx"/>
-      <FolderComponent foldername="Components"/>
+      {/* <FileComponent fileName="Index.tsx"/> */}
+    {fileTree.children?.map((item,idx) => (
+      <>
+        {item.isFolder ? 
+        <>
+        <FolderComponent key={idx} foldername={item.name}/>
+        <h1 className="ml-5">{item.children?.map((item,idx) => <FileComponent key={idx} fileName={item.name} />)}</h1>
+        </>
+        :
+        <FileComponent key={idx} fileName={item.name}/>
+        }
+      </>
+    ))}
+      {/* <FolderComponent foldername="Components"/> */}
     </>
   )
 }
--- src/data/fileTree.tsx
+++ src/data/fileTree.tsx
@@ -2,11 +2,18 @@ import type { IFileTree } from "../interfaces";
 
 export const fileTree : IFileTree = {
   name: "VS Code Clone",
-    isFolder: true,
+  isFolder: true,
   children:[
     {
         name: "node_modules",
-        isFolder: true
+        isFolder: true,
+        children:[
+          {name:".vite",isFolder:true, children:[
+              {name:"Mersal.tsx", isFolder:false}
+          ]},
+          {name:"react.js",isFolder:false},
+          {name:"HBD.js",isFolder:false}
+        ]
     },
     {
         name: "index.html",
