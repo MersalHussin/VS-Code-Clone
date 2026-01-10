71055f3e6aa1676976a0d3947626dc3929b44884
mersalhussin <mersalhussin@gmail.com>
2026-01-10 02:00:52 +0200

    Display Nested Child With AI

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,39 @@ import FileComponent from "./components/FileComponent";
 import FolderComponent from "./components/FolderComponent";
 // import FolderComponent from "./components/FolderComponent";
 import { fileTree } from "./data/fileTree";
+import type { IFileTree } from "./interfaces";
+
+
+interface RecursiveTreeProps{
+  item: IFileTree
+}
 
 function App() {
+
+  const RecusiveTree = ({item}: RecursiveTreeProps) =>{
+    if(!item.isFolder){
+      return <FileComponent  fileName={item.name}/>
+    }
+    return(
+      <div>
+        <FolderComponent foldername={item.name}/>
+        <div className="ml-5 border-l border-gray-300 pl-2">
+          {item.children?.map((child,idx) => <RecusiveTree key={idx} item={child}/>)}
+        </div>
+      </div>
+    )
+  }
+
+//   function factorial(file:IFileTree){
+//   if(file.children?.length == 0) return file
+//   return file.children?.map((file) => file.isFolder ? <FolderComponent/> : <FileComponent /> )
+// }
   return (
     <>
       {/* <FileComponent fileName="Index.tsx"/> */}
     {fileTree.children?.map((item,idx) => (
       <>
-        {item.isFolder ? 
-        <>
-        <FolderComponent key={idx} foldername={item.name}/>
-        <h1 className="ml-5">{item.children?.map((item,idx) => <FileComponent key={idx} fileName={item.name} />)}</h1>
-        </>
-        :
-        <FileComponent key={idx} fileName={item.name}/>
-        }
+      <RecusiveTree key={idx} item={item}/>
       </>
     ))}
       {/* <FolderComponent foldername="Components"/> */}
