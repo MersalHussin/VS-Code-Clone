f26e4604756c032e7cb55efd5841195c579f8b48
mersalhussin <mersalhussin@gmail.com>
2026-01-12 03:55:14 +0200

    Make Constans  Folder

--- src/components/RenderFileIcon.tsx
+++ src/components/RenderFileIcon.tsx
@@ -1,28 +1,10 @@
+import { extensionIconPath } from "../consts";
 import IconImage from "./IconImage";
 
 interface IProps{
 filename: string
 isOpenFolder?: boolean
 isfolder?: boolean
-}
-
-const extensionIconPath: Record<string,string> = {
-    // Files
-    tsx:"/icons/typescript",
-    ts:"/icons/react_ts",
-    js:"/icons/react",
-    jsx:"/icons/javascript",
-    html:"/icons/html",
-    default_file:"/icons/default",
-    
-    // Folders
-    node_modules:"/icons/folder-node",
-    vscode:"/icons/folder-vscode",
-    public:"/icons/folder-public",
-    src:"/icons/folder-src",
-    components:"/icons/folder-components",
-    default_folder:"/icons/folder-default",
-
 }
 
 const RenderFileIcon = ({filename,isOpenFolder,isfolder}: IProps ) => {
---
+++ src/consts/index.ts
@@ -0,0 +1,17 @@
+export const extensionIconPath: Record<string,string> = {
+    // Files
+    tsx:"/icons/typescript",
+    ts:"/icons/react_ts",
+    js:"/icons/react",
+    jsx:"/icons/javascript",
+    html:"/icons/html",
+    default_file:"/icons/default",
+    
+    // Folders
+    node_modules:"/icons/folder-node",
+    vscode:"/icons/folder-vscode",
+    public:"/icons/folder-public",
+    src:"/icons/folder-src",
+    components:"/icons/folder-components",
+    default_folder:"/icons/folder-default",
+}
