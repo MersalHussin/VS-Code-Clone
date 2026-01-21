e121d66247e2340b41f4c746f654b010d8b7703a
mersalhussin <mersalhussin@gmail.com>
2026-01-21 14:48:32 +0200

    Context Menu Refrence

--- src/components/ui/DropMenu.tsx
+++ src/components/ui/DropMenu.tsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+
 interface IProps{
 postion: {x:number, y:number}
 
@@ -5,12 +7,20 @@ postion: {x:number, y:number}
 
 
 const DropMenu = ({postion}: IProps ) => {
+    const menuRef = useRef<HTMLDivElement>(null)
+    console.log(menuRef);
     return (
+        <div ref= {menuRef}>
         <ul className="bg-white text-black" style={{position:"absolute",left:postion.x,top:postion.y }}>
             <li className="bg-white">close</li>
             <li className="bg-white">CloseAll</li>
         </ul>
+        </div>
     );
 }
 
 export default DropMenu;
+
+//  ** UseEffect
+//  ** Click Event
+//  ** Menu Redf => UseRef Done
